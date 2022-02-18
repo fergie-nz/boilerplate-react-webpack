8a69297abf8f81e408f11f00f521bb0b29101825
Fergieboi <90807420+Fergieboi@users.noreply.github.com>
2022-02-18 16:33:15 +1300

    updates the test suites to correct naming convention beginning with this test:...

--- countNeighbours.test.js
+++ countNeighbours.test.js
@@ -1,46 +1,46 @@
-test('Testing countNeighbouts for 0,0 coordinate on dummy array', () => {
+test('the countNeighbours function counts the correct amount of neighbours for the 0,0 position', () => {
   const array = functions.testArray(10)
   const actual = countNeighbourFunctions.countNeighbours(0,0,array)
   const expected = 1
   expect(actual).toBe(expected)
 })
 
-test('Testing countNeighbouts for 1,1 coordinate on dummy array', () => {
+test('the countNeighbours function counts the correct amount of neighbours for the 1,1 position', () => {
   const array = functions.testArray(10)
   const actual = countNeighbourFunctions.countNeighbours(1,1,array)
   const expected = 3
   expect(actual).toBe(expected)
 })
 
-test('Testing 2 neighbour cell which is currently alive', () => {
+test('the countNeighbours function counts the correct amount of neighbours for the 2,2 position', () => {
   const array = functions.testArray(10)
   const actual = countNeighbourFunctions.countNeighbours(2,2,array)
   const expected = 2
   expect(actual).toBe(expected)
 })
 
-test('Testing countNeighbouts for 4,0 coordinate on dummy array', () => {
+test('the countNeighbours function counts the correct amount of neighbours for the 4,0 position', () => {
   const array = functions.testArray(10)
   const actual = countNeighbourFunctions.countNeighbours(4,0,array)
   const expected = 0  
   expect(actual).toBe(expected)
 })
 
-test('Testing countNeighbouts for 0,4 coordinate on dummy array', () => {
+test('the countNeighbours function counts the correct amount of neighbours for the 0,4 position', () => {
   const array = functions.testArray(10)
   const actual = countNeighbourFunctions.countNeighbours(0,4,array)
   const expected = 0
   expect(actual).toBe(expected)
 })
 
-test('Testing countNeighbouts for 4,4 coordinate on dummy array', () => {
+test('the countNeighbours function counts the correct amount of neighbours for the 4,4 position', () => {
   const array = functions.testArray(10)
   const actual = countNeighbourFunctions.countNeighbours(4,4,array)
   const expected = 0  
   expect(actual).toBe(expected)
 })
 
-test('Testing 3 neighbour coordinate not currently containing cell', () => {
+test('the countNeighbours function counts the correct amount of neighbours for the 1,2 position', () => {
   const array = functions.testArray(10)
   const actual = countNeighbourFunctions.countNeighbours(1,2,array)
   const expected = 3  
--- functions.test.js
+++ functions.test.js
@@ -1,57 +1,57 @@
-test('test setup working', () => {
+test('the testing suite is working', () => {
   expect(true).toBeTruthy()
 })
 
-test('testing array length', () => {
+test('createArray function renders the right length', () => {
   const array = state.createArray(10)
   const actual = array.length
   const expected = 10
   expect(actual).toBe(expected)
 })
 
-test('testing array depth', () => {
+test('createArray function renders the right depth', () => {
   const array = state.createArray(10)
   const actual = array[0].length
   const expected = 10
   expect(actual).toBe(expected)
 })
 
-test('testing array internal count', () => {
+test('the countNeighbours function counts the right amount of neighbours in an internal cell', () => {
   const array = state.testArray(10)
   const actual = state.countNeighbours(2,2,array)
   const expected = 2
   expect(actual).toBe(expected)
 })
 
-test('testing array external count', () => {
+test('the countNeighbours function counts the right amount of neighbours in an external cell', () => {
   const array = state.testArray(10)
   const actual = state.countNeighbours(0,0,array)
   const expected = 1
   expect(actual).toBe(expected)
 })
 
-test('testing countNeighbours for wrong results', () => {
+test('the countNeighbours function counts the right amount of neighbours for a near edge cell', () => {
   const array = state.testArray(10)
   const actual = state.countNeighbours(1,1,array)
   const expected = 3
   expect(actual).toBe(expected)
 })
 
-test('test the changing of state is correct for internal cells', () => {
+test('the changeState function works correctly for the standard test array', () => {
   const array = state.testArray(10)
   const expected = state.testNewState(10)
   const actual = state.changeState(array)
   expect(actual).toStrictEqual(expected)
 })
 
-test('deep cloning function working', () => {
+test('the deepClone function is working as expected for cloning nested arrays', () => {
   const array = state.testArray(10)
   const expected = array
   const actual = _.cloneDeep(array)
   expect(actual).toStrictEqual(expected)    
 })
 
-test('changing deep clone states results in no longer clone', () => {
+test('the validity of the above function by proving a false case', () => {
   const array = state.testArray(10)
   const actual = _.cloneDeep(array)
   actual[0][0].living = false
